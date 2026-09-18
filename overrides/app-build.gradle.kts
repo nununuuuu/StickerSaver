e29299aa4722ea8e8ec43ded38d49979a9b3b0e9
uuuuuu <126788456+nununuuuu@users.noreply.github.com>
2026-09-18 23:19:56 +0800

    Bump Sticker Saver to 0.2.7

--- overrides/app-build.gradle.kts
+++ overrides/app-build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.local.threadssticker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.2.6"
+        versionCode = 9
+        versionName = "0.2.7"
     }
 
     signingConfigs {
