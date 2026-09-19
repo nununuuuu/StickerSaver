7585080fbf64fdbf9ac2b908150ab409f42b8e53
uuuuuu <126788456+nununuuuu@users.noreply.github.com>
2026-09-19 11:50:03 +0800

    Bump version to 0.3.6

--- overrides/app-build.gradle.kts
+++ overrides/app-build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.local.threadssticker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.3.5"
+        versionCode = 18
+        versionName = "0.3.6"
     }
 
     signingConfigs {
